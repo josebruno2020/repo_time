f931cc3aa9b8a69d73ee9dc0a5e8a402daa72684
José Bruno <josebrunocampanholi@gmail.com>
2024-11-05 18:00:06 -0300

    :hammer: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   process.env.TZ = 'America/Sao_Paulo';
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors();
   const port = 3200;
   await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
